ea5e9f150619c510cbeb75442e4aa4afda347bb0
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2020-12-01 10:25:28 -0500

    Fix bug when running in parallel

--- project2/calc_snpcounts.sh
+++ project2/calc_snpcounts.sh
@@ -13,8 +13,8 @@ kbase=`basename $kuwaiti _genomic.fna`
 dir=nucmer/$s6base/$kbase
 filebase=$dir/$s6base.$kbase
 mkdir -p $dir
-nucmer -t 2 --delta $filebase $s6 $kuwaiti
+nucmer  --delta $filebase $s6 $kuwaiti
 show-snps -I -T -q -C $filebase > $filebase.snps           
-echo -n $s6base$'\t'$kbase$'\t'
-awk '{ if ($5 >=20) print $0}' $filebase.snps \
-        | awk '{ if ($6 >= 25) print $0}' | wc -l
+count=$(awk '{ if ($5 >=20) print $0}' $filebase.snps \
+        | awk '{ if ($6 >= 25) print $0}' | wc -l )
+echo $s6base$'\t'$kbase$'\t'$count
