0fe9e8538a517c459cdf1740840f789a316a4548
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2020-12-02 13:17:51 -0500

    add create_submit_list.sh

---
+++ project1/create_submit_list.sh
@@ -0,0 +1,6 @@
+cut -f 3 singapore_samples_srafull.txt | perl -ne 'BEGIN { print "--env ERR\t--output ASM\n" }
+        chomp;
+        print join("\t",
+                "$_",
+                "gs://asm-ngs-aprasad/project1/step2/asm/$_.asm.fa"), "\n"
+        '
