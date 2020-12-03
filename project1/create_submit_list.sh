3250f0a0a18b02bca769c017de9746ae4fc6ee4e
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2020-12-02 21:58:20 -0500

    bug fix for $USER

--- project1/create_submit_list.sh
+++ project1/create_submit_list.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 cut -f 3 singapore_samples_srafull.txt | perl -ne 'BEGIN { print "--env ERR\t--output ASM\n" }
         chomp;
         print join("\t",
                 "$_",
-                "gs://asm-ngs-$USER/project1/step2/asm/$_.asm.fa"), "\n"
+                "gs://asm-ngs-'$USER'/project1/step2/asm/$_.asm.fa"), "\n"
         '
